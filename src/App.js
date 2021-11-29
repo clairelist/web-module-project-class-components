a096bd6ffa40f59f23524b5cb44ae46292656701
Claire List <claire.list2501@gmail.com>
2021-11-29 14:29:29 -0700

    laying out needful code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+//...once again, DATA AKA IMPORT section
+//!'AKA' is DEPRECATED; use PSEUD instead!
 import React from 'react';
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
+
+//LOGIC PSEUD FUNCTION section
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -13,4 +20,5 @@ class App extends React.Component {
   }
 }
 
+//RETURN PSEUD 'EXPORT' section
 export default App;
